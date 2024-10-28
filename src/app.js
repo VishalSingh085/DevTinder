deb12f6a48e8c668f7044e4ca5a22e49bafa08bc
vivekraj singh thakur <vsinghthakur187@gmail.com>
2024-10-28 11:58:04 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 const express = require("express");
-
-
 const app = express();
 app.use("/test",(req,res)=>{
    res.send("test code request is run");
-   
 })
 // app.use("/",(req,res)=>{
 //     res.send(" request is run");
